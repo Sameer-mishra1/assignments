47656b4f288b2ba7c15fbc85ad0ed6140de344e5
Sameer-mishra1 <77093405+Sameer-mishra1@users.noreply.github.com>
2023-12-12 10:39:16 +0530

    Initial commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,32 @@
 
 function isAnagram(str1, str2) {
 
+  num1 = str1.length;
+  num2 = str2.length;
+
+  if (num1 != num2) {
+    return false;
+  }
+
+  rearrangedStr1 = str1.toLowerCase().split('').sort().join('');
+  rearrangedStr2 = str2.toLowerCase().split('').sort().join('');
+
+
+  return rearrangedStr1 === rearrangedStr2;
+
 }
+
+//   str1 = str1.toLowerCase();
+//   str2 = str2.toLowerCase();
+
+//   // going through every letter
+//   for ( let char of str1) {
+
+//     if ( str2.indexOf(char) === -1) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 module.exports = isAnagram;
