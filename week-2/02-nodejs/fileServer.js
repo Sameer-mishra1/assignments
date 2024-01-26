62b5a08e1db94036ace86a4c5eb9e0017ac20a29
Sameer-mishra1 <77093405+Sameer-mishra1@users.noreply.github.com>
2024-01-26 09:55:47 +0530

    Solved fileServer & todoServer

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -6,16 +6,42 @@
     Response: 200 OK with an array of file names in JSON format.
     Example: GET http://localhost:3000/files
   2. GET /file/:filename - Returns content of given file by name
-     Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-     Example: GET http://localhost:3000/file/example.txt
+    Description: Use the filename from the request path parameter to read the file from `./files/` directory
+    Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+    Example: GET http://localhost:3000/file/example.txt
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+const { log } = require('console');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 
+const directoryPath = 'E:/100x-dev-cohort/assignments/week-2/02-nodejs/files/'
+
+app.get('/files', (req, res) => {
+  fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+    if (err) {
+      return res.status(500).json({error: "Sorr, could not retrieve the files for some reason. Try later!"})
+    }
+    res.json(files);
+  })
+});
+
+app.get('/file/:filename', (req, res) => {
+  const filePath = path.join(__dirname, './files/', req.params.filename);
+
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data)
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = app;
